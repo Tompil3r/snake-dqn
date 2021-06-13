a667029f524e2f17b153c605b313dfc74b312d62
Tompil3r <tompil3r@gmail.com>
2021-06-13 18:03:33 +0300

    training session

--- train_agent.py
+++ train_agent.py
@@ -16,10 +16,12 @@ eps_min_value = 0.01
 training_nb_steps = 50_000
 eps_decay_nb_steps = training_nb_steps // 5
 
+weights_path = 'agent_misc/snake_model_weights.h5'
 
 model = Model.build_model(state_shape, nb_actions, name='Snake-Model')
 agent = Agent.build_agent(model, nb_actions, memory_limit, eps_max_value, eps_min_value, eps_decay_nb_steps)
 agent.compile('adam', metrics=['mae'])
+agent.load_weights(weights_path)
 
 training_history = agent.fit(env, training_nb_steps, visualize=False, verbose=1)
-agent.save_weights('snake_model_weights.h5', overwrite=True)
+agent.save_weights(weights_path, overwrite=True)
