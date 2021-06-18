847d792ebe3cf60724aa874d064a9ce6ad7ab596
Tompil3r <tompil3r@gmail.com>
2021-06-18 19:32:57 +0300

    trained model and added code for continuous training

--- train_agent.py
+++ train_agent.py
@@ -1,17 +1,23 @@
 from env import SnakeEnv
 from agent import DQNAgent
 import numpy as np
+import os
 
 env = SnakeEnv()
 
+weights_path = 'model_weights.h5'
+
 state_shape = env.observation_space.shape
 nb_actions = env.action_space.nb_actions
-training_steps = 10000
+training_steps = 20_000_000
 
 agent = DQNAgent(state_shape, nb_actions, eps_decay_steps=training_steps)
 
 agent.create_experiences(env, 1000)
 
+if os.path.exists(weights_path):
+    agent.load_weights(weights_path)
+
 history = agent.fit(env, training_steps, batch_size=64)
 
-agent.save_weights('model_weights.h5')
+agent.save_weights(weights_path)
