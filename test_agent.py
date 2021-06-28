21422d68ace4c47e2d6b26a8fad647635c821733
Tompil3r <tompil3r@gmail.com>
2021-06-28 13:49:28 +0300

    updated test_agent file for new models

--- test_agent.py
+++ test_agent.py
@@ -1,6 +1,7 @@
 from env import SnakeEnv
 from agent import DQNAgent
 import numpy as np
+import models
 
 
 env = SnakeEnv()
@@ -8,7 +9,9 @@ env = SnakeEnv()
 state_shape = env.observation_space.shape
 nb_actions = env.action_space.nb_actions
 
-agent = DQNAgent(state_shape, nb_actions)
+model = models.build_model_1(state_shape, nb_actions)
+target_model = models.build_model_1(state_shape, nb_actions)
+agent = DQNAgent(state_shape, nb_actions, model=model, target_model=target_model)
 
 agent.load_weights('model_weights.h5')
 scores = agent.test(env, 10, visualize=True)
